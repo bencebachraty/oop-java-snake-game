0630353c28fa24f387cacc5b559ff271cef5a3ca
Thao Phuong Lam <lphuongt930906@gmail.com>
2018-12-12 11:12:44 +0100

    Displays text on screen

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -2,6 +2,11 @@ package com.codecool.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,6 +24,13 @@ public class Main extends Application {
         primaryStage.setScene(mainScene);
         primaryStage.show();
 
+        Text t = new Text();
+        t.setText("Total Life: ");
+        t.setX(40);
+        t.setY(50);
+        t.setFill(Color.BLUE);
+        t.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        game.getChildren().add(t);
         game.start();
     }
 
